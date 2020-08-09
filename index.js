fb59cf2cdae7c8ad03c4fea0049da4ecfe3de80a
unknown <jmsmidhunkrishna@gmail.com>
2020-08-09 21:19:38 +0530

    added adding option

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const telegraf = require('telegraf');
 const mongoose = require('mongoose');
-const Link = require('./model/schema')
+const Link = require('./model/schema');
+require('dotenv').config();
 
-const Dburl = "mongodb+srv://testbotuser:testbotuser@links.qt7ih.mongodb.net/Link?retryWrites=true&w=majority";
-var dbconnect = mongoose.connect(Dburl)
+const Dburl = process.env.DB_url;
+var dbconnect = mongoose.connect(Dburl, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => {
         console.log("db connected");
     })
@@ -11,78 +12,103 @@ var dbconnect = mongoose.connect(Dburl)
         console.log(err);
     })
 
-const Bot = new telegraf("1364496503:AAF-8IOnBfC1iU3pEQHGLHp4my_Pz2mjqb0");
+const Bot = new telegraf(process.env.Bot_Token);
 
 Bot.start((res) => res.reply("Welcome"));
 Bot.hears('hi', (res) => res.reply("how are you?"));
+Bot.command('help', (ctx) => {
+    ctx.reply("List of Available commands are:\n- /List_all : Displays all available books \n- /request <book name> : Returns the link of the ebook if exists\n"
+
+    );
+})
 Bot.command('request', (ctx) => {
-    ctx.reply('Hello');
+    //ctx.reply('Hello');
     let message = ctx["update"]["message"]["text"];
     let bname = message.substring(9, message.length);
-    //console.log(bname);
+    if (bname != "") {
+        //console.log(bname);
+        mongoose.connect(Dburl, { useNewUrlParser: true, useUnifiedTopology: true })
+            .then((result) => {
+                Link.find()
+                    .then((result) => {
+                        console.log(result);
+                        result.forEach((data) => {
+
+                            if (data["title"] == bname) {
+                                //console.log(data["title"]);
+                                //console.log(data["link"]);
+                                ctx.reply(data["link"]);
+                            }
+                        })
+
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    })
+                //ctx.reply(Link.find());
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+    else {
+        ctx.reply("Please follow the format\n /request <book name>");
+    }
+}
+);
+//this command displays the list of all books in the database
+Bot.command('List_all', (ctx) => {
+    let message = 'Available books are:\n';
+
     mongoose.connect(Dburl, { useNewUrlParser: true, useUnifiedTopology: true })
         .then((result) => {
-            // console.log(result);
+
             Link.find()
                 .then((result) => {
                     console.log(result);
                     result.forEach((data) => {
-
-                        if (data["title"] == bname) {
-                            //console.log(data["title"]);
-                            //console.log(data["link"]);
-                            ctx.reply(data["link"]);
-                        }
+                        message += data["title"] + '\n';
                     })
+                    ctx.reply(message);
 
                 })
                 .catch((err) => {
                     console.log(err);
                 })
-            //ctx.reply(Link.find());
+
         })
         .catch((err) => {
             console.log(err);
         })
 
-}
-
-);
-
-Bot.command('List_all', (ctx) => {
-    let message='';
-    
-    mongoose.connect(Dburl, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then((result) => {
-            
-            Link.find()
-                .then((result) => {
-                    console.log(result);
-                    result.forEach((data)=>{
-                        message+=data["title"]+'\n';
-                            ctx.reply(message);
-                    })
-                    
-                })
-                .catch((err) => {
-                    console.log(err);
-                })
-            //ctx.reply(Link.find());
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-   
 }
 
 );
 
 Bot.command('add', (ctx) => {
-    const data = new Link({
-        title: "duckett",
-        link: "https://drive.google.com/file/d/1eXntJnCaHYsjyUwK2FUFQaiVu5JaQcyD/view?usp=sharing"
-    });
-    data.save();
+    let addmessage = ctx["update"]["message"]["text"];
+    let newbname = addmessage.substring(5, addmessage.length);
+    console.log(newbname);
 
-})
+    Bot.on('message', (data) => {
+        // console.log(bname);
+        // console.log(data);
+        const dbdata = new Link({
+            title: newbname,
+            link: data.update.message.text
+        });
+        dbdata.save();
+        data.deleteMessage();
+        ctx.deleteMessage();
+    });
+
+
+
+
+
+
+
+});
+
+
 Bot.launch();
